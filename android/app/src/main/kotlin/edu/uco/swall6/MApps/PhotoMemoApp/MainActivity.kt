a9c99408c8649e7b08841a590eb8c2e5aa2cc7fc
SeanAlexWall <seanalexwall@gmail.com>
2021-03-07 13:19:39 -0600

    init

---
+++ android/app/src/main/kotlin/edu/uco/swall6/MApps/PhotoMemoApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.swall6.MApps.PhotoMemoApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/signin_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget{
+  static const routeName = "/signInScreen";
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SignInState();
+  }
+  
+}
+
+class _SignInState extends State<SignInScreen> {
+  _Controller con;
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
+  void render(fn){
+    setState(fn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In"),
+      ),
+      body: Text("SignIn"),
+    );
+  }
+
+}
+
+
+class _Controller{
+  _SignInState state;
+  _Controller(this.state);
+}
